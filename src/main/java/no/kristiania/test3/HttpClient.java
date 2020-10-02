bce71952ab9d8148bf8bccf5daf9ffd35d508330
robinsoras <robinsoras@hotmail.com>
2020-10-02 13:13:03 +0200

    http-client proof-of-concept

---
+++ src/main/java/no/kristiania/test3/HttpClient.java
@@ -0,0 +1,27 @@
+package no.kristiania.test3;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class HttpClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("urlecho.appspot.com", 80);
+
+        // Format as specified in the HTTP specification
+        // Each line is separated by \r\n (CRLF)
+        // The request ends with empty line (\r\n\r\n)
+        String request = "GET /echo?status=200&body=Hello%20world! HTTP/1.1\r\n" +
+        "Host: urlecho.appspot.com\r\n\r\n";
+        // Writes data to the server
+        socket.getOutputStream().write(request.getBytes());
+
+        // Reads one BYTE at a time, until there is nothing more to read
+        // (c = socket.getInputStream().read()) != -1 means
+        // Assign the next value of "read()" to c and check if it's not -1
+        // (-1 means end of data)
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            System.out.print((char)c);
+        }
+    }
+}
