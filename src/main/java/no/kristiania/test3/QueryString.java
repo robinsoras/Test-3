b066b95f5bfabc08324fb0aa9bb5d6042092eaaf
robinsoras <robinsoras@hotmail.com>
2020-10-02 12:01:29 +0200

    solved red test, added new test, running red

--- src/main/java/no/kristiania/test3/QueryString.java
+++ src/main/java/no/kristiania/test3/QueryString.java
@@ -2,13 +2,20 @@ package no.kristiania.test3;
 
 public class QueryString {
     private final String value;
+    private final String parameterName;
 
     public QueryString(String queryString) {
         int equalsPos = queryString.indexOf("=");
         value = queryString.substring(equalsPos + 1);
+        parameterName = queryString.substring(0, equalsPos);
     }
 
     public String getParameter(String name) {
-        return value;
+
+        if (name.equals(parameterName)) {
+            return value;
+        }
+
+        return null;
     }
 }
--- src/test/java/no/kristiania/test3/QueryStringTest.java
+++ src/test/java/no/kristiania/test3/QueryStringTest.java
@@ -23,4 +23,11 @@ public class QueryStringTest {
         assertEquals(null, queryString.getParameter("status"));
         assertEquals("Hello", queryString.getParameter("text"));
     }
+
+    @Test
+    void shouldHandleMultipleParameters() {
+        QueryString queryString = new QueryString("text=Hello&status=200");
+        assertEquals("200", queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
+    }
 }
