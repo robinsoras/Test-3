f2bf49c776954aa72c771606c218f84aca6ca370
robinsoras <robinsoras@hotmail.com>
2020-10-02 17:37:40 +0200

    First test green, added second test, red

--- src/main/java/no/kristiania/test3/HttpClient.java
+++ src/main/java/no/kristiania/test3/HttpClient.java
@@ -6,6 +6,8 @@ import java.net.Socket;
 
 public class HttpClient {
 
+    private int statusCode = 200;
+
     public HttpClient(String hostname, int port, String requestTarget) throws IOException {
         Socket socket = new Socket(hostname, port);
 
@@ -13,13 +15,23 @@ public class HttpClient {
                 "Host: " + hostname + "\r\n\r\n";
         socket.getOutputStream().write(request.getBytes());
 
+        StringBuilder line = new StringBuilder();
         int c;
         while ((c = socket.getInputStream().read()) != -1) {
-            System.out.print((char)c);
+            if (c == '\n') break;
+            line.append((char)c);
         }
+        System.out.println(line);
+        String[] parts = line.toString().split(" ");
+        statusCode = Integer.parseInt(parts[1]);
     }
 
     public static void main(String[] args) throws IOException {
         new HttpClient("urlecho.appspot.com", 80, "/echo?status=200&body=Hello%20world!");
     }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
 }
--- src/test/java/no/kristiania/test3/HttpClientTest.java
+++ src/test/java/no/kristiania/test3/HttpClientTest.java
@@ -1,5 +1,23 @@
 package no.kristiania.test3;
 
-class HttpClientTest {
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+    @Test
+    void shouldReturnSuccessfulStatusCode() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo");
+        assertEquals(200, client.getStatusCode());
+    }
+
+
+    @Test
+    void shouldReturnErrorfulStatusCode() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo?status=404");
+        assertEquals(404, client.getStatusCode());
+    }
 }
